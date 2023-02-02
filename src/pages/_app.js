b97b9bc8ad12d08600c38d537fa86691055aff20
jakkapatB <jakkapat.bo@kkumail.com>
2023-02-03 02:37:33 +0700

    push navbar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import * as React from "react";
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react";
+import Navbar from "components/Navbar";
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Navbar/>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
---
+++ src/pages/listscore.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function listsocre() {
+  return (
+    <div>listsocre</div>
+  )
+}
+
+export default listsocre
