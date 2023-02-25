6ab5bf9222c179a97c96968c8e007f9e6f928fc4
643040191-3 <chotipat.s@kkumail.com>
2023-02-25 11:32:08 +0700

    Update viewscore.js

--- src/pages/viewscore.js
+++ src/pages/viewscore.js
@@ -540,7 +540,7 @@ function scoreboard() {
             {/* <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span> */}
             {/* <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span> */}
             {/* <span>{("0" + ((time / 10) % 100)).slice(-2)}</span> */}
-            {timeToString}
+            Viewer
           </Text>
         </Center>
       </Box>
